47019d914afc67101c60d066d414bc8e5c64363a
Faisal Latif <faisal.latif1295@gmail.com>
2023-08-04 16:43:19 +0500

    first commit

---
+++ class2.js
@@ -0,0 +1,95 @@
+"use strict";
+console.log('Faisal Latif');
+// use of let, var and const 
+let myName = 'Faisal';
+/* use of camel-case */
+var myNameis = "Burhan";
+/* use of pascal-case */
+const MyNameIs = `Muhammad`;
+// use of letter, number, dollar signs, underscores //
+let userName123 = `I am learning variables`;
+var userName$ = `I am student of PIAIC`;
+const userName_ = `I am learning Typescript`;
+//use of Primitive data types, String, Number, Boolean, Undefined, Null.
+let happyBirthday = `Muhammad Burhan`; //string
+var num1 = 10; //number
+var num2 = 20; //number
+var num3 = 30; //number
+const isMarried = false; //boolean
+const isHappybirthday = true; //boolean
+const isJobless = false; //boolean
+let unassigned; //undefined
+var value1 = null;
+var value2 = null;
+var value3 = null;
+let empty = null;
+//Template literals or Template string
+var myNameis = `Daniyal`;
+var hello1 = "Ali" + myNameis;
+var hello2 = `Ali${hello1}`;
+var hello3 = `Muhammad${myNameis}`;
+//analyzing and Modifying data type
+var num1 = 1;
+var a = "1";
+//Arithmatic operators
+//Addition
+let n1 = 1;
+let n2 = 2;
+let result1 = n1 + n2;
+const str1 = "1";
+const str2 = "2";
+const result2 = str1 + str2;
+//Subtraction
+let n3 = 10;
+let n4 = 5;
+let result3 = n3 - n4;
+//Multiplication
+let n5 = 2;
+let n6 = 3;
+let result4 = n5 * n6;
+//Division
+let n7 = 10;
+let n8 = 5;
+let result5 = n7 / n8;
+//Exponentiation Operator
+var e1 = 10;
+var e2 = 2;
+var result6 = e1 ** e2;
+//Modulus Operator
+var m1 = 10;
+var m2 = 5;
+var result7 = m1 % m2;
+var m3 = 9;
+var m4 = 5;
+var result8 = m3 % m4;
+//Assignment Operator
+var a1 = 5;
+a1 += 5;
+var a2 = 10;
+a2 -= 5;
+var a3 = 15;
+a3 *= 2;
+var a4 = 6;
+a4 /= 2;
+//Comparison Operator
+var com1 = 10;
+var com2 = 5;
+var output1 = com1 == com2;
+var output2 = com1 === com2;
+var output3 = com1 > com2;
+var output4 = com1 < com2;
+var output5 = com1 <= com2;
+var output6 = com1 >= com2;
+var output7 = com1 != com2;
+//Logical Operator
+//F && F = F, F || F = F
+//F && T = F, F || T = T
+//T && F = F, T || F = T
+//T && T = T, T || T = T
+let log1 = 8;
+let log2 = 7;
+let output9 = log1 <= log2 && log2 == log1;
+let output10 = log1 > log2 && log1 !== log2;
+let output11 = log2 <= log1 || log1 < log2;
+let output12 = log1 != log2 || log2 < log1;
+console.log(output11);
---
+++ class2.ts
@@ -0,0 +1,93 @@
+console.log('Faisal Latif');
+// use of let, var and const 
+let myName:string='Faisal';
+/* use of camel-case */
+var myNameis:string="Burhan";
+/* use of pascal-case */
+const MyNameIs:string=`Muhammad`;
+// use of letter, number, dollar signs, underscores //
+let userName123:string=`I am learning variables`;
+var userName$:string=`I am student of PIAIC`;
+const userName_:string=`I am learning Typescript`;
+//use of Primitive data types, String, Number, Boolean, Undefined, Null.
+let happyBirthday:string=`Muhammad Burhan`; //string
+var num1:number=10; //number
+var num2:number=20; //number
+var num3:number=30; //number
+const isMarried:boolean=false; //boolean
+const isHappybirthday:boolean=true; //boolean
+const isJobless:boolean=false; //boolean
+let unassigned; //undefined
+var value1:null=null;
+var value2:null=null;
+var value3:null=null;
+let empty:null=null;
+//Template literals or Template string
+var myNameis=`Daniyal`;
+var hello1="Ali"+myNameis;
+var hello2=`Ali${hello1}`;
+var hello3:string=`Muhammad${myNameis}`;
+//analyzing and Modifying data type
+var num1=1;
+var a="1"; 
+//Arithmatic operators
+//Addition
+let n1=1;
+let n2=2;
+let result1=n1+n2;
+const str1="1";
+const str2="2";
+const result2=str1+str2;
+//Subtraction
+let n3=10;
+let n4=5;
+let result3=n3-n4;
+//Multiplication
+let n5=2;
+let n6=3;
+let result4=n5*n6;
+//Division
+let n7=10;
+let n8=5;
+let result5=n7/n8;
+//Exponentiation Operator
+var e1=10;
+var e2=2;
+var result6=e1**e2;
+//Modulus Operator
+var m1=10;
+var m2=5;
+var result7=m1%m2;
+var m3=9;
+var m4=5;
+var result8=m3%m4;
+//Assignment Operator
+var a1=5;
+a1 +=5;
+var a2=10;
+a2 -=5;
+var a3=15;
+a3 *=2;
+var a4=6
+a4 /=2;
+//Comparison Operator
+var com1:number=10;
+var com2:number=5;
+var output1:boolean=com1==com2;
+var output2:boolean=com1===com2;
+var output3:boolean=com1>com2;
+var output4:boolean=com1<com2;
+var output5:boolean=com1<=com2;
+var output6:boolean=com1>=com2;
+var output7:boolean=com1!=com2;
+//Logical Operator
+//F && F = F, F || F = F
+//F && T = F, F || T = T
+//T && F = F, T || F = T
+//T && T = T, T || T = T
+let log1=8;
+let log2=7;
+let output9:boolean=log1<=log2 && log2==log1;
+let output10:boolean=log1>log2 && log1!==log2;
+let output11:boolean=log2<=log1 || log1<log2;
+let output12:boolean=log1!=log2 || log2<log1;
